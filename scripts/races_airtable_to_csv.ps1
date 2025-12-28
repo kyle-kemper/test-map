8cdea0144b515a5c2914a09756188e3dae815838
kyle-kemper <105694784+kyle-kemper@users.noreply.github.com>
2025-12-27 21:09:58 -0500

    Update and rename airtable_to_csv.ps1 to races_airtable_to_csv.ps1

--- scripts/races_airtable_to_csv.ps1
+++ scripts/races_airtable_to_csv.ps1
@@ -4,7 +4,7 @@
 $AIRTABLE_TOKEN = $env:AIRTABLE_TOKEN
 $BASE_ID = "appN2ESAm2bjJ4esB"
 $TABLE_NAME = "races_kylecopy"
-$OUTPUT_CSV = "airtable_fetched.csv"
+$OUTPUT_CSV = "races_airtable_fetched.csv"
 
 # ============================
 # HEADERS
